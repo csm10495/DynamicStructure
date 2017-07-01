5f7fbf5ea8a1ca93e2fc938fa623fdf918003e05
csm10495 <csm10495@gmail.com>
2017-06-30 19:11:37 -0700

    Add anonymous support along with docstring injection support.

--- DynamicStructure/dynamic_structure.py
+++ DynamicStructure/dynamic_structure.py
@@ -24,7 +24,7 @@ class S(DynamicStructure):
     ]
 '''
 
-def getStructureType(fieldTuple, buffer, parent=Structure, pack=1):
+def getStructureType(fieldTuple, buffer, parent=Structure, pack=1, anonymous=None):
     '''
     adds the fieldTuple to the given parent using the buffer.
 
@@ -32,6 +32,10 @@ def getStructureType(fieldTuple, buffer, parent=Structure, pack=1):
                                         the buffer at this point onward as parameters.
                                             They can be used by a lambda to calculate out this field's size.
     '''
+
+    if anonymous is None:
+        anonymous = []
+
     if len(fieldTuple) >= 2 and not inspect.isclass(fieldTuple[1]):
         # extended?
         parentFilled = parent().fill(buffer)
@@ -49,22 +53,33 @@ def getStructureType(fieldTuple, buffer, parent=Structure, pack=1):
         # Remember, we can't make TmpStructure with bitfields that don't complete bytes
         #    since ctypes will pack to the nearest byte
 
+    # handle anonymous
+    qualifiedAnonymous = [] # only anon in this structure
+    for fieldName in anonymous:
+        if fieldName == fieldTuple[0]:
+            qualifiedAnonymous.append(fieldName)
+
     class TmpStructure(parent):
         _pack_ = pack
+        _anonymous_ = qualifiedAnonymous
         _fields_ = [
                 fieldTuple
             ]
 
     return TmpStructure
 
-def getDynamicStructure(fields, buffer=None, pack=1):
+def getDynamicStructure(fields, buffer=None, pack=1, anonymous=None, docstring=''):
     '''
     gets a self-defining structure with the given fields, buffer, pack.
     '''
+    if anonymous is None:
+        anonymous = []
+
     class BuildStructure(Structure):
         '''
         define this in here to not be messed up when called again
         '''
+        _pack_ = pack
         def getBytes(self):
             return cast(byref(self), POINTER(c_uint8))
 
@@ -76,13 +91,45 @@ def getDynamicStructure(fields, buffer=None, pack=1):
                 self.getBytes()[i] = buffer[i]
             return self
 
+        def getAllFields(self):
+            fields = []
+            for i in type(self).mro():
+                if hasattr(i, '_fields_'):
+                    fields += i._fields_
+
+            return fields[::-1]
+
+        def getUpdated(self, fieldName, fieldType):
+            fieldsCopy = self.getAllFields()
+            found = False
+            for idx, i in enumerate(fieldsCopy):
+                i = list(i)
+                if i[0] == fieldName:
+                    i[1] = fieldType
+                    fieldsCopy[idx] = tuple(i)
+                    found = True
+
+                    break
+
+            if not found:
+                return False
+
+            class TmpStructure(Structure):
+                _pack_ = self._pack_
+                _fields_ = fieldsCopy
+
+            return TmpStructure
+                    
+
     if buffer is None:
         buffer = []
 
     for idx, fieldTuple in enumerate(fields):
-        BuildStructure = getStructureType(fieldTuple, buffer, BuildStructure, pack=pack)
-        
-    return BuildStructure().fill(buffer)
+        BuildStructure = getStructureType(fieldTuple, buffer, BuildStructure, pack=pack, anonymous=anonymous)
+    
+    BuildStructure.__doc__ = inspect.cleandoc(docstring)
+    retStruct = BuildStructure().fill(buffer)
+    return retStruct
 
 def getCorrectVersionStructure(buffer):
     '''
@@ -125,7 +172,13 @@ if __name__ == '__main__':
                 ('VerStruct', lambda self, buffer: getCorrectVersionStructure(buffer)), # The buffer at this point is telling us the structure needed
                 #('BitField1', c_uint8, 4),  # Broken right now since bitfields are expanding in the initial substructure (so this is 1 byte instead of 4 bits)
                 #('BitField2', c_uint8, 4),  # Broken right now since bitfields are expanding in the initial substructure (so this is 1 byte instead of 4 bits)
-            ], buf, pack=1)
+            ], buf, pack=1, anonymous=['VerStruct'], docstring="""
+            Brief:
+                Test Structure
+
+            Author(s):
+                Charles Machalow
+            """)
     t2 = time()
 
     print ("sizeof(s) == %d" % sizeof(s))
